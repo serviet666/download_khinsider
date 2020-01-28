5be07eb3aa9b82d0969dfcb85f618f552a4c774c
Nicolas Amiral <n.amiral@oodrive.com>
2020-01-28 17:21:38 +0100

    optimization

--- background.js
+++ background.js
@@ -1,26 +1,19 @@
-chrome.webNavigation.onCompleted.addListener(details => {
-	chrome.pageAction.show(details.tabId);
-}, {
-	url: [{
-		urlPrefix: "https://downloads.khinsider.com/game-soundtracks/album/"
-	}]
-});
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	localStorage[sender.tab.id] = JSON.stringify({
-		'songlist': request.songlist,
-		'title': request.title
-	});
-});
-chrome.pageAction.onClicked.addListener(tab => {
-	if(localStorage[tab.id] !== undefined || localStorage[tab.id] !== null) {
-		var ost = JSON.parse(localStorage[tab.id]);
-		ost.songlist.forEach(idx => {
+	if(request.type === 'all') {
+		request.songlist.forEach(idx => {
+			let filename = decodeURI(decodeURI(idx.substring(idx.lastIndexOf('/') + 1)));
+			console.log(idx);
+			console.log(filename);
 			chrome.downloads.download({
 				'url': idx,
-				'filename': ost.title + '/' + decodeURI(decodeURI(idx.substring(idx.lastIndexOf('/') + 1)))
+				'filename': request.title + '/' + filename
 			});
 		});
+	} else if(request.type === 'song') {
+		let filename = decodeURI(decodeURI(request.song.substring(request.song.lastIndexOf('/') + 1)));
+		chrome.downloads.download({
+			'url': song,
+			'filename': request.title + '/' + filename
+		});
 	}
-});
-chrome.tabs.onRemoved.addListener(tabId => {
 });
--- download_ost_khinsider.js
+++ download_ost_khinsider.js
@@ -1,30 +1,37 @@
 $(document).ready(() => {
-	var urls = new Set();
-	var songlist = $('.playlistDownloadSong a')
+	let urls = new Set();
+	let songlist = $('.playlistDownloadSong a')
 	songlist.each(idx => {
 		urls.add($(songlist[idx]).attr('href'));
 	});
-	var title = $('h2')[0].textContent;
-	var urlsCount = urls.size;
-	var index = 0;
-	var mp3s = new Array();
-	urls.forEach((value, key, set) => {
-		$.get('https://downloads.khinsider.com' + value, data => {
-			var url = $(data).find('audio')[0].src;
-			mp3s.push(url);
-			if(set.size === ++index) {
-				chrome.runtime.sendMessage({
-					'songlist': mp3s,
-					'title': title
-				});
-				mp3s = new Array();
-			}
-		});
-	});
+	let title = $('h2')[0].textContent;
+	let urlsCount = urls.size;
+	let mp3s = new Array();
 	$('.albumMassDownload a').attr('href', '#');
 	$('.albumMassDownload a').on('click', e => {
-		chrome.runtime.sendMessage({
-			
+		urls.forEach((value, key, set) => {
+			$.get('https://downloads.khinsider.com' + value, data => {
+				let url = $(data).find('audio')[0].src;
+				if(!mp3s.some(el => url === el) && set.size === mp3s.push(url)) {
+					chrome.runtime.sendMessage({
+						'type': all,
+						'songlist': mp3s,
+						'title': title
+					});
+					mp3s = new Array();
+				}
+			});
 		});
+	});
+	$('.playlistDownloadSong a').on('click', e => {
+		console.log(e.currentTarget.href);
+		$.get(e.currentTarget.href, data => {
+			chrome.runtime.sendMessage({
+				'type': 'song',
+				'song': $(data).find('audio')[0].src,
+				'title': title
+			});
+		});
+		return false;
 	});
 });
